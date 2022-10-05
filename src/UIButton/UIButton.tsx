1e307eb34ac949211f6cab40cc6f0562cdbc9dfc
bombin78 <bombin78@mail.ru>
2022-10-05 21:34:10 +0300

    Компонент "UIButton"

--- src/UIButton/UIButton.tsx
+++ src/UIButton/UIButton.tsx
@@ -1,16 +1,25 @@
 import React, {FC} from 'react';
+import './UIButton.css'
+
 
 export interface UIButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 	color: string;
+	size?: string;
 }
 
 const UIButton: FC<UIButtonProps> = ({ 
 	children,
 	color,
+	size,
 	...props 
 }) => {
+	const styles = [
+		'ui-button',
+		size === 'big' ? 'big-btn' : null,
+	];
+
 	return (
-		<button {...props} style={{ color }}>
+		<button {...props} className={styles.join(' ')} style={{ color }}>
 			{ children }
 		</button>
 	);
